9500b286abdd1cd7a21b38aa84bfed026f53b2d5
chrisbg <archangelbg@abv.bg>
2020-03-22 13:13:42 +0200

    added login and register icons

--- src/pages/PageLogin.vue
+++ src/pages/PageLogin.vue
@@ -7,7 +7,7 @@
           <p class="subtitle has-text-grey">Please login to proceed.</p>
           <div class="box">
             <figure class="avatar">
-                <img src="https://placehold.it/128x128">
+                <img src="../assets/img/Login-icon.png">
             </figure>
             <form>
               <div class="field">
