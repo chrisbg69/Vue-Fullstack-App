4ed4b1621545ebe76bf1321066a94266366dac60
chrisbg <archangelbg@abv.bg>
2020-03-29 22:56:27 +0300

    add geolocation from IP

--- server/index.js
+++ server/index.js
@@ -18,7 +18,8 @@ const meetupsRoutes = require('./routes/meetups'),
       usersRoutes = require('./routes/users'),
       threadsRoutes = require('./routes/threads'),
       postsRoutes = require('./routes/posts'),
-      categoriesRoutes = require('./routes/categories');
+      categoriesRoutes = require('./routes/categories'),
+      apiRoutes = require('./routes/api');
 
 mongoose.connect(config.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
   .then(() => console.log('DB Connected!'))
@@ -34,7 +35,7 @@ require('./socket')(io)
 
 app.use(bodyParser.json()); 
 
-
+app.use('/api/v1', apiRoutes);
 app.use('/api/v1/meetups', meetupsRoutes);
 app.use('/api/v1/users', usersRoutes);
 app.use('/api/v1/posts', postsRoutes);
