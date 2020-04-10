84de5d09be2fbe388b9eaf7ebdc39477ffbf62a0
chrisbg <archangelbg@abv.bg>
2020-04-10 22:34:37 +0300

    move filters to own folder

--- src/main.js
+++ src/main.js
@@ -5,13 +5,13 @@ import store from './store';
 import vuelidate from 'vuelidate';
 import Toasted from 'vue-toasted';
 import AppSocket from './plugins/socket';
-
+import filters from './filters';
 
 import AppHero from './components/shared/AppHero';
 import AppSpinner from './components/shared/AppSpinner';
 import Paginate from 'vuejs-paginate';
 
-import moment from 'moment';
+
 
 
 
@@ -25,24 +25,8 @@ Vue.use(vuelidate);
 Vue.use(Toasted);
 Vue.use(AppSocket, {connection: 'http://localhost:3001'});
 
+filters()
 
-Vue.filter('capitalize', function (value) {
-  if(value && typeof value === 'string') {
-    return value.charAt(0).toUpperCase() + value.slice(1);
-  }
-  return '';
-});
-
-Vue.filter('formatDate', function (value, formatType = 'LL') {
-  if(!value) return '';
-  return moment(value).format(formatType);
-});
-
-
-Vue.filter('fromNow', function (value) {
-  if(!value) return '';
-  return moment(value).fromNow();
-});
 
 new Vue({  
   router,
